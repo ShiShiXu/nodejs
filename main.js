c7550ffc342fafc55c9ddd961e5a06cdfd0cf95d
Allen_Tsui <1093806808@qq.com>
2016-08-24 17:59:37 +0800

    ...

--- main.js
+++ main.js
@@ -8,4 +8,3 @@ hello = new Hello();
 hello.setName(' Node.js 提供了exports 和 require 两个对象:其中 exports 是模块公开的接口，' +
     'require 用于从外部获取一个模块的接口，即所获取模块的 exports 对象。');
 hello.sayHello();
-console.log("create/add github");
